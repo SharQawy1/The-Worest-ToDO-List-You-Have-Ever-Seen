ef521f2c5f6285046b280a17cf25c40433d529f9
SharQawy1 <124384551+SharQawy1@users.noreply.github.com>
2023-03-09 21:14:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,78 @@
 let input = document.querySelector("input");
-let btn = document.querySelector(".submit");
-let tasksBox = document.querySelector(".tasks");
+let submit = document.querySelector(".submit");
+let tasksDiv = document.querySelector(".tasks");
+let tasks = [];
 
-if (window.localStorage.getItem("tasksStorage") == null) {
-  console.log("Add A New Note");
-  let tasks = [];
-  btn.onclick = function () {
-    document.querySelector(".tasks").innerHTML = "";
-    if (input.value == "") {
-      console.log("empty");
-    } else {
-      tasks.push(input.value);
-      input.value = "";
-    }
-    for (let i = 0; i < tasks.length; i++) {
-      let task = document.createElement("div");
-      task.classList.add("task");
-      task.textContent = tasks[i];
-      tasksBox.appendChild(task);
-    }
-    window.localStorage.clear;
-    tasks.forEach(function (ele) {});
-    window.localStorage.setItem("tasksStorage", tasks);
-  };
-} else {
-  let tasks = window.localStorage.getItem("tasksStorage").split(","); ///////////////////////
-  btn.onclick = function () {
-    if (input.value == "") {
-      console.log("empty");
-    } else {
-      tasks.push(input.value);
-      input.value = "";
-    }
-    document.querySelector(".tasks").innerHTML = "";
-    for (let i = 0; i < tasks.length; i++) {
-      let task = document.createElement("div");
-      task.classList.add("task");
-      task.textContent = tasks[i];
-      tasksBox.appendChild(task);
-    }
-    window.localStorage.clear;
-    window.localStorage.setItem("tasksStorage", tasks);
-  };
-  let deleteBtn = document.querySelector(".task:nth-of-type(1)");
-  deleteBtn.onclick = function () {
-    deleteBtn.parentElement.remove();
-  };
+dataFromStorage();
+
+if (localStorage.getItem("tasks")) {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
 }
 
-onload = function () {
-  btn.click();
+//Add a task
+submit.onclick = () => {
+  if (input.value != "") {
+    addTaskToArray(input.value);
+    input.value = "";
+  }
 };
 
-let task = document.querySelector(".task");
-let deleteBtn = document.querySelector(".delete");
-deleteBtn.onclick = function () {
-  window.localStorage.clear();
-  window.location.reload();
-};
+//Click On Task Element
+tasksDiv.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+    deleteTask(e.target.parentElement.getAttribute("data-id"));
+  }
+});
+
+function addTaskToArray(taskText) {
+  const task = {
+    id: Date.now(),
+    title: taskText,
+    completed: false,
+  };
+  tasks.push(task);
+  //Add Tasks To Page
+  add(tasks);
+  dataToStorage(tasks);
+}
+
+function add(tasks) {
+  //[1]Empty Tasks Array
+  tasksDiv.innerHTML = "";
+  //[2]Looping On Array Of Tasks
+  tasks.forEach((task) => {
+    let div = document.createElement("div");
+    div.className = "task";
+    //[3]Check If Task Is Done
+    if (task.completed) {
+      div.className = "task done";
+    }
+    div.setAttribute("data-id", task.id);
+    div.appendChild(document.createTextNode(task.title));
+    //[4]Delete Button
+    let delSpan = document.createElement("span");
+    delSpan.className = "delete";
+    delSpan.appendChild(document.createTextNode("Delete"));
+    div.appendChild(delSpan);
+    //[5]Add Task Div To Tasks
+    tasksDiv.appendChild(div);
+  });
+}
+
+function dataToStorage(tasks) {
+  window.localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function dataFromStorage() {
+  let data = window.localStorage.getItem("tasks");
+  if (data) {
+    let tasks = JSON.parse(data);
+    add(tasks);
+  }
+}
+
+function deleteTask(taskId) {
+  tasks = tasks.filter((task) => task.id != taskId);
+  dataToStorage(tasks);
+}
